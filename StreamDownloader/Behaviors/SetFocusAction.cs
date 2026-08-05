3aefb68b2c64c91772c8cf28ee8fc02e4f10aa48
rot <appl.rot13@gmail.com>
2026-08-05 17:49:50 +0900

    Set focus to the text box after Add/Clear

---
+++ StreamDownloader/Behaviors/SetFocusAction.cs
@@ -0,0 +1,26 @@
+﻿
+namespace StreamDownloader.Behaviors;
+
+using Microsoft.Xaml.Behaviors;
+
+using System.Windows;
+
+public class SetFocusAction : TriggerAction<DependencyObject>
+{
+    public static readonly DependencyProperty TargetElementProperty = DependencyProperty.Register(
+        nameof(TargetElement),
+        typeof(UIElement),
+        typeof(SetFocusAction),
+        new PropertyMetadata(null));
+        
+    public UIElement TargetElement
+    {
+        get => (UIElement)this.GetValue(TargetElementProperty);
+        set => this.SetValue(TargetElementProperty, value);
+    }
+
+    protected override void Invoke(object parameter)
+    {
+        this.TargetElement?.Focus();
+    }
+}
